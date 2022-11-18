0cdfe145e6a4ac90c7eae18034ff6bee0820881b
Yann <yannnda97@gmail.com>
2022-11-18 09:04:23 +0100

    import data ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,49 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const photographerCard = document.createElement("article");
+    photographerCard.innerHTML = `
+        <a href="photographer.html?id=${id}">
+            <img class="portrait-img" src="${picture}" alt ="${name}" title = "picture of" + "${name}">
+            <h2 class="portrait-name">${name}</h2>
+        </a>
+            <p class="portrait-city">${city}, ${country}</p>
+            <p class="portrait-tagline">${tagline}</p>
+            <p class="portrait-price">${price}€/jour</p>
+        `;
+    return photographerCard;
+  }
+
+  function getPhotographerHeader() {
+    const photographerHeader = document.querySelector(".photographer-header");
+    const getPhotographerHeaderBody = document.createElement("div");
+
+    getPhotographerHeaderBody.classList.add("photographer-header-container");
+    getPhotographerHeaderBody.innerHTML = `
+        <div class="photographer-header-container-portrait">
+            <h2 class="portrait-name">${name}</h2>
+            <p class="portrait-city">${city}, ${country}</p>
+            <p class="portrait-tagline">${tagline}</p>
+        </div>
+        <button class="contact-button" onclick="displayModal()">Contactez-moi</button>
+        <img class="portrait-img" src="${picture}" alt="${id}" title="picture of" + "${id}">
+        `;
+    return photographerHeaderContainer;
+  }
+
+  return {
+    name,
+    id,
+    city,
+    country,
+    tagline,
+    price,
+    portrait,
+    picture,
+    getUserCardDOM,
+    getPhotographerHeader,
+  };
 }
